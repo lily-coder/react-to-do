a3dfd8c5fd347167ad06de804b4d85824d4d7936
lily-coder <lilyondie@gmail.com>
2021-09-29 11:27:57 +0300

    Add react app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component file
+import TodoContainer from "./functionBased/components/TodoContainer";
+import "./functionBased/App.css";
+import { BrowserRouter as Router } from "react-router-dom"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
